bb717be81f9e2d00e90a49e26d3d4faed92043b7
Vitorvzp <Vitorvzp722@gmail.com>
2025-05-15 23:06:00 -0300

    Atualização em 15/05/2025 23:05

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+git pull origin main --allow-unrelated-histories
 # Configuração do usuário (adicione estas linhas)
 git config --local user.email "Vitorvzp722@gmail.com"
 git config --local user.name "Vitorvzp"
 
-# Comandos existentes
 git add .
 git commit -m "Atualização em $(date +'%d/%m/%Y %H:%M')"
 git push origin main
